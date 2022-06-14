60f1783f53a74e6ac78a2df2aac5d44bbb46e022
zorrenho <zorrenho@gmail.com>
2022-06-14 08:47:08 -0600

    favicon

--- Views/template.php
+++ Views/template.php
@@ -17,7 +17,7 @@ class Template
       <meta charset="UTF-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <link rel=”shortcut icon” href=”favicon.ico” mce_href=”favicon.ico” type=”image/x-icon” />
+      <link rel="shortcut icon" href="favicon.ico" mce_href="favicon.ico" type="image/x-icon" />
       <link rel="stylesheet" href="<?php echo URL; ?>Views/assets/css/menu-sticky.css">
       <link rel="stylesheet" href="<?php echo URL; ?>Views/assets/css/slider.css">
       <link rel="stylesheet" href="<?php echo URL; ?>Views/assets/css/general-style.css">
