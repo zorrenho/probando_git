ce39e77fcfb9e1d044cb76e920d918d599cd2916
zorrenho <zorrenho@gmail.com>
2022-06-14 08:25:10 -0600

    modificacion

--- Config/Config.php
+++ Config/Config.php
@@ -2,7 +2,7 @@
 // DB CREDENCIALES DE USUARIO.
 define('DB_HOST','localhost');
 define('DB_USER','root');
-define('DB_PASS','admin');
+define('DB_PASS','');
 define('DB_NAME','proyecto');
 define('DB_TIPO','mysql');
 define('DB_CHARSET','utf8');
--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 //VARIABLES GLOBALES
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', realpath(dirname(__FILE__)) . DS);
-define('URL', "http://localhost/CRUD_MVC/");
+define('URL', "http://localhost/probando_git/");
 
 require_once "Config/Config.php";
 require_once "Config/Autoload.php";
